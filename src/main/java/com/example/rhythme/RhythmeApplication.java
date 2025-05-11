7e262fa31fe4c8481c7060f4632c5040b852da3c
dayeone1 <s2312@e-mirim.hs.kr>
2025-05-12 00:25:17 +0900

    spring 파일 셋팅

---
+++ src/main/java/com/example/rhythme/RhythmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhythme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhythmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhythmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rhythme/RhythmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rhythme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhythmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
