b9f61fa5b1aab1b7f98f428348d3c098e7c5be4e
이민서 <sonichoko10@gmail.com>
2025-06-01 22:25:00 +0900

    Revert "기본 폴더 및 파일 생성"

--- src/main/java/com/example/rhythme/dao/UserDAO.java
+++
@@ -1,5 +0,0 @@
-package com.example.rhythme.dao;
-
-public interface UserDAO {
-
-}
--- src/main/java/com/example/rhythme/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.rhythme.service;
-
-import com.example.rhythme.dto.UserDTO;
-
-public interface UserService {
-
-}
