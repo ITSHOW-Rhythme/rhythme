c38eb08ee132b8daea753ef80c5a8da5ac19accd
이민서 <sonichoko10@gmail.com>
2025-06-17 01:00:46 +0900

    Resolve conflict in AllSongmapper.xml

--- src/main/java/com/example/rhythme/dto/AllSongDTO.java
+++ src/main/java/com/example/rhythme/dto/AllSongDTO.java
@@ -3,7 +3,11 @@ package com.example.rhythme.dto;
 import lombok.Data;
 
 @Data
-public class AllSongDTO {
+<<<<<<< HEAD:src/main/java/com/example/rhythme/dto/SongDTO.java
+public class SongDTO {
+=======
+public class SongDTO {
+>>>>>>> f41560bd168787b96c13bcefacc5867ab1a0855d:src/main/java/com/example/rhythme/dto/SongDTO.java
     private Integer song_id;
     private String title;
     private String artist;
