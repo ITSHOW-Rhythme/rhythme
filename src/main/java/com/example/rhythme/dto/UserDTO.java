1c60d93802c593750f4bb1707e4b1a753698e39e
Yeji Yun <142556812+2zji@users.noreply.github.com>
2025-06-17 20:26:40 +0900

    전 userDTO 가져오기

--- src/main/java/com/example/rhythme/dto/UserDTO.java
+++ src/main/java/com/example/rhythme/dto/UserDTO.java
@@ -1,11 +1,14 @@
 package com.example.rhythme.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 
 public class UserDTO {
-    private Integer user_id;
+    private Integer user_Id;
     private String username;
     private String password;
 }
