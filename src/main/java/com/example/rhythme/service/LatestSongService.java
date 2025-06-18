b4bd1ed7c13056aeccb59a507039bd63e127b694
이민서 <sonichoko10@gmail.com>
2025-06-19 02:48:35 +0900

    최근 학습한 노래 저장 API 구현

--- src/main/java/com/example/rhythme/service/LatestSongService.java
+++ src/main/java/com/example/rhythme/service/LatestSongService.java
@@ -2,6 +2,10 @@ package com.example.rhythme.service;
 
 import com.example.rhythme.dto.LatestSongDTO;
 
+import java.time.LocalDateTime;
+
 public interface LatestSongService {
     LatestSongDTO loadRecentSong(int userId);
+
+    void saveUserSongHistory(int userId, int songId);
 }
